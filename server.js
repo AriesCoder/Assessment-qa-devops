6311fc15ca662190355b89f9c1656e5b2c036745
Trang Lam <ms.tranglam@gmail.com>
2022-02-25 10:15:05 -0800

    try to get error notification

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/api/player', (req, res) => {
 })
 
 const port = process.env.PORT || 3000
-
+app.use(rollbar.errorHandler())
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
